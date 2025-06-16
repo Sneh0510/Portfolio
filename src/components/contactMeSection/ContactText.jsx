0a0a37bc54f7dcb52e9321e7b14e32ee35c9c589
Sneh Yadav <snehyadav091@gmail.com>
2025-06-16 09:43:11 +0530

    fix: remove unnecessary line break in ContactText component

--- src/components/contactMeSection/ContactText.jsx
+++ src/components/contactMeSection/ContactText.jsx
@@ -3,8 +3,7 @@ const ContactText = () => {
     <div>
       <h2 className="mb-4 text-3xl text-orange">Get In Touch</h2>
       <p>
-        Currently open to new opportunities and eager 
-        <br />
+        Currently open to new opportunities and eager
         to bring my skills to a dynamic team. Let’s connect!
       </p>
     </div>
